0196a5c6bb4e7ace79d227cfcb69beb0bae9e684
kyohei.uda <fcm1kyohei@gmail.com>
2020-08-13 13:18:31 +0000

    ボタン機能を追加したい

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,13 @@ class User < ApplicationRecord
     has_secure_password
     
     has_many :microposts
+    has_many :favorites
+    has_many :favorite, through: :favorites, source: :micropost
     has_many :relationships
     has_many :followings, through: :relationships, source: :follow
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
-    
+
     def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,14 @@ Rails.application.routes.draw do
     collection do
       get :search
     end
+    member do
+      get :likes
+    end
   end
     
   
   resources :microposts, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
