cba7982d4b954558eb9ce394e9bb050c19ccdb85
kyohei.uda <fcm1kyohei@gmail.com>
2020-08-18 11:24:42 +0000

    削除機能についての質問

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  has_many :favorites
+  has_many :favorites, class_name: 'Favorite', foreign_key: 'micropost_id'
   has_many :users, through: :favorites, source: :user
   
   validates :content, presence: true, length: { maximum: 255 }
